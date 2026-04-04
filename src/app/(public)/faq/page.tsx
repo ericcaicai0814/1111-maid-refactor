3604088c34fbdda2bb1265611dcc054b5767e342
ericcai0814 <d4070707@gmail.com>
2026-04-04 22:53:31 +0800

    feat: 實作 FAQ 頁 accordion 元件（階段 2c）

--- src/app/(public)/faq/page.tsx
+++ src/app/(public)/faq/page.tsx
@@ -1,8 +1,5 @@
+import { FAQAccordionSection } from '@/components/sections/faq';
+
 export default function FAQPage() {
-  return (
-    <main>
-      <h1>常見問題</h1>
-      <p>外籍幫傭申請 FAQ</p>
-    </main>
-  );
+  return <FAQAccordionSection />;
 }
